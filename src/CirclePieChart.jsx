5325c0825ca3284e7400500c69859555c64a2b73
Квентин Дорвард <kventindorvard@MacBook-Pro-Kventin.local>
2020-03-27 13:29:49 +0300

    add show animation

--- src/CirclePieChart.jsx
+++ src/CirclePieChart.jsx
@@ -1,12 +1,22 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 
-import Pie from "./Pie";
-import { useCalcCommonSum } from "./customHooks/calcCommonSum";
-import { dynamicCalcPieChart } from "./utils/dynamicCalcPieChart";
-import { getColorForCategory } from './constants'
-import formatNumber from './utils/formatNumber'
+import Pie from './Pie';
 
-const CirclePieChart = ({ data, width, height, radius, thickness,text,activeCategory }) => {
+
+import { useCalcCommonSum } from './customHooks/calcCommonSum';
+import { dynamicCalcPieChart } from './utils/dynamicCalcPieChart';
+import { getColorForCategory } from './constants';
+import formatNumber from './utils/formatNumber';
+
+const CirclePieChart = ({
+  data,
+  width,
+  height,
+  radius,
+  thickness,
+  text,
+  activeCategory
+}) => {
   const commonSum = useCalcCommonSum(data);
   const [proccesedData, setProcessedData] = useState([]);
 
@@ -14,17 +24,17 @@ const CirclePieChart = ({ data, width, height, radius, thickness,text,activeCate
     setProcessedData(dynamicCalcPieChart(data, radius, commonSum));
   }, [commonSum, data, radius]);
 
-  return (  
+  let delay = 0;
+  return (
     <svg
       width={width}
       height={height}
-      version="1.1"
-      xmlns="http://www.w3.org/2000/svg"
-      xmlnsXlink="http://www.w3.org/1999/xlink"
-    >
-      <mask id="myMask">
-        <rect x="0" y="0" width={width} height={height} fill="white" />
-        <circle cx={width / 2} cy={height / 2} r={radius} fill="while" />
+      version='1.1'
+      xmlns='http://www.w3.org/2000/svg'
+      xmlnsXlink='http://www.w3.org/1999/xlink'>
+      <mask id='myMask'>
+        <rect x='0' y='0' width={width} height={height} fill='white' />
+        <circle cx={width / 2} cy={height / 2} r={radius} fill='while' />
       </mask>
 
       {!proccesedData && (
@@ -32,28 +42,36 @@ const CirclePieChart = ({ data, width, height, radius, thickness,text,activeCate
           I love SVG!
         </text>
       )}
-
       {proccesedData &&
-        proccesedData.map(i => (
-          <Pie
-            id={i.id}
-            key={i.id}
-            isActive={activeCategory === i.category}
-            cx={width / 2}
-            cy={height / 2}
-            className={`pie-${i.id}`}
-            color={getColorForCategory[i.category] || '#d6d6d6'}
-            radius={radius}
-            thickness={thickness}
-            startPosition={i.startPosition}
-            value={i.value}
-            mask="url(#myMask)"
-          />
-      ))}
+        proccesedData.map(i => {
+          delay += 300;
 
-      <use xlinkHref="#myMask" />
-      <text textAnchor="middle"  x={width / 2} y={height / 2}>{text}</text>
-      <text textAnchor="middle"  x={width / 2} y={(height / 2) + 20}>{formatNumber(commonSum)} ₽</text>
+          return (
+            <Pie
+              d={delay}
+              id={i.id}
+              key={i.id}
+              isActive={activeCategory === i.category}
+              cx={width / 2}
+              cy={height / 2}
+              className={`pie-${i.id}`}
+              color={getColorForCategory[i.category] || '#d6d6d6'}
+              radius={radius}
+              thickness={thickness}
+              startPosition={i.startPosition}
+              value={i.value}
+              mask='url(#myMask)'
+            />
+          );
+        })}
+
+      <use xlinkHref='#myMask' />
+      <text textAnchor='middle' x={width / 2} y={height / 2}>
+        {text}
+      </text>
+      <text textAnchor='middle' x={width / 2} y={height / 2 + 20}>
+        {formatNumber(commonSum)} ₽
+      </text>
     </svg>
   );
 };
--- src/Pie.jsx
+++ src/Pie.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import { Transition } from 'react-transition-group';
 
 const Pie = ({
   id,
@@ -12,28 +12,68 @@ const Pie = ({
   thickness,
   color,
   mask,
-  isActive
+  isActive = false,
+  d
 }) => {
+  const [animateActive, setAnimateState] = useState(false);
+  const [offAnimate,setOffAnimate] = useState(true);
+
   const rotate = `rotate(-90 ${cx} ${cy})`;
   const maxStrokeLength = 2 * 3.14 * radius;
   const startStrokeArray = `${value} ${maxStrokeLength}`;
+  const resetStyles = {}
 
+  const transitionStyles = {
+    entering: {
+      strokeDasharray: startStrokeArray,
+      strokeDashoffset: startPosition,
+    },
+    entered: {
+      strokeDasharray: startStrokeArray,
+      strokeDashoffset: startPosition,
+    },
+    exiting: {
+      strokeDasharray: `0 ${maxStrokeLength}`,
+      strokeDashoffset: startPosition,
+    },
+    exited: {
+      strokeDasharray: `0 ${maxStrokeLength}`,
+      strokeDashoffset: startPosition,
+    },
+  };
+
+  useEffect(() => {
+    setTimeout(() => setAnimateState(true), 100);
+    setTimeout(() => setOffAnimate(false), 200);
+  }, [setAnimateState,setOffAnimate]);
+
+  console.log({ isActive });
   
-  return ( 
-    <circle 
-      className={isActive ? className + ' pie__scale': className}
-      r={radius}
-      cx={cx}
-      cy={cy}
-      fill={'none'}
-      transform={rotate}
-      stroke={color}
-      strokeWidth={thickness}
-      strokeDasharray={startStrokeArray}
-      strokeDashoffset={startPosition}
-      mask={mask}
-    />
-   );
-}
- 
+  return (
+    <Transition in={animateActive}>
+      {state => {
+        return (
+          <circle
+            className={isActive ? className + ' pie__scale': className}
+            style={{
+              transition: `all ease 450ms ${offAnimate ? d : 0}ms`,
+              ...transitionStyles[state]
+            }}
+            r={radius}
+            cx={cx}
+            cy={cy}
+            fill={'none'}
+            transform={rotate}
+            stroke={color}
+            strokeWidth={thickness}
+            strokeDasharray={startStrokeArray}
+            strokeDashoffset={startPosition}
+            mask={mask}
+          />
+        );
+      }}
+    </Transition>
+  );
+};
+
 export default Pie;
